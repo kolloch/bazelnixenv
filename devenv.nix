9bb4706ba4e4e490d482d754f3b6a83c9b8b9517
Peter Kolloch <peter.kolloch@nexxiot.com>
2023-10-04 18:02:40 +0000

    [rust/hello] can reproduce build problem

--- devenv.nix
+++ devenv.nix
@@ -1,31 +1,61 @@
-{ pkgs, ... }:
+{pkgs, ...}: let
+  bazel =
+    pkgs.writeScriptBin "bazel"
+    ''${pkgs.bazelisk}/bin/bazelisk "$@"'';
+in {
+  # If you don't want to install nix, you can use it via a docker container in VSCode.
+  # devcontainer.enable = true;
+  containers."shell".copyToRoot = null;
+  # Fancy prompt.
+  starship.enable = true;
 
-{
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
   # https://devenv.sh/languages/
   languages = {
-    c.enable = true;
-    nix.enable = true;
     rust.enable = true;
-    javascript.enable = true;
     typescript.enable = true;
   };
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.bazel_6 pkgs.nodePackages.pnpm pkgs.gcc ];
+  packages = [
+    # Include it or use system git?
+    pkgs.git
+    pkgs.cacert
+
+    # Bazel
+    pkgs.bazel_6
+    pkgs.bazel-buildtools
+
+    # TypeScript
+    pkgs.nodePackages.pnpm
+  ];
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
+  # scripts.hello.exec = "echo hello from $GREET";
 
   enterShell = ''
-    hello
-    git --version
   '';
 
   # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks = {
+    # shellcheck.enable = true;
+    # shfmt.enable = true;
+
+    # nix reformat
+    # alejandra.enable = true;
+    # nix lint
+    # statix.enable = true;
+
+    rustfmt.enable = true;
+
+    # markdownlint.enable = true;
+    # https://zimbatm.github.io/mdsh/
+    # mdsh.enable = true;
+
+    prettier.enable = true;
+
+    # yamllint.enable = true;
+  };
 
   # https://devenv.sh/processes/
   # processes.ping.exec = "ping example.com";
